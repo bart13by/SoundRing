285b5ffb5c219f9894bc9fbe63fd140516c904d2
Patrick Bryant <patrick.bryant@emory.edu>
2023-10-10 22:55:12 -0400

    Synced ring position with audio play/pause/sync

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
 // app.js
+function fastForward(){
+	const motion = document.getElementById("motion");
+	const rotation = document.getElementById("rotation");
+	motion.setAttribute("dur", "24s");
+	rotation.setAttribute("dur", "24s");
+}
+
+function doPause(s){
+	document.getElementById('svg-container').pauseAnimations();
+}
+
+function doPlay(e){
+	document.getElementById('svg-container').unpauseAnimations();
+}
+
+function doSeek(e){
+	const t = e.target;
+	document.getElementById('svg-container').setCurrentTime(t.currentTime);
+}
