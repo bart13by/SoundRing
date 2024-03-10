18f7dd7203ef32a669a6b6c5d3e17b4826686655
Patrick Bryant <patrick.bryant@emory.edu>
2024-03-09 22:08:17 -0500

    Sync'd migrant birds with audio timer. Lots of bugging/debugging/rebugging, but it seems to work now

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,7 @@ function doPause(e){
 	When the audio pauses, pause the animations 
 	*/
 	document.getElementById('svg-container').pauseAnimations();
+	pauseApp();
 }
 
 function doPlay(e){
@@ -13,15 +14,24 @@ function doPlay(e){
 	When the audio plays, play the animations 
 	*/
 	document.getElementById('svg-container').unpauseAnimations();
+	resumeApp();
+	
+	
+}
+function doSeeking(e){
+	console.log(`seeking `);
+	pauseApp();
 }
 
 function doSeek(e){
+	console.log(` = seeked = `);
 	/*
 	When the audio seeks, set the animation currentTime to the audio currentTime
 	*/
 	
 	const t = e.target; // event target is the HTMLMediaElement playing the audio
 	document.getElementById('svg-container').setCurrentTime(t.currentTime);
+	resumeApp();
 }
 
 function doLoaded(e){
@@ -34,14 +44,16 @@ function doLoaded(e){
 	// setTimeout executes in a thread, waiting a full execution cycle. 
 	// Which is virtually 0 time so probably not necessary.
 	setTimeout(() => {
-		console.log(e.target.readyState);
+		//console.log(e.target.readyState);
 		// "loaded" class is hidden via CSS
 		loadScreen.className = "loaded";
 
 		// Ring starts hidden, but that didn't fix the bug in Safari :-(
 		document.getElementById('wheel').style.display = 'flex';
-
+		
+		runBirdApp();
 		// We may need to force-trigger the play event to make sure the animation starts. 
 		e.target.play();
+
 	}, "1");
 }
