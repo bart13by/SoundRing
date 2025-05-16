5c7cc830eb205f099c144bab3c2192937c2e91bc
Patrick Bryant <patrick.bryant@emory.edu>
2025-05-16 15:03:51 -0400

    updated iframes; added script to update iframes

---
+++ updateiframes.sh
@@ -0,0 +1,3 @@
+wget --convert-links -nH -nd -O atlas.html https://birdpress.adagia.org/wind-key/
+wget --convert-links -nH -nd -O key.html https://birdpress.adagia.org/firmament-key/
+wget --convert-links -nH -nd -O linernotes.html  https://birdpress.adagia.org/liner-notes-continuous/
